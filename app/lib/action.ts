aeff60d2669e9bd43d18407fa62a6123410cb0d9
185182 <185182@cn.chervongroup.com>
2024-04-14 22:59:38 +0800

    feat: 11-add error and not-found

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -28,10 +28,16 @@ export async function createInvoice(formData: FormData) {
 
   console.log('Creating invoice...', customerId, amountInCents, status, date);
 
-  await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date}) 
-    `;
+  try {
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date}) 
+      `;
+  } catch (error) {
+    return {
+      error: 'Database Error: Failed to create invoice.',
+    };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -45,21 +51,35 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
   const amountInCents = amount * 100;
-
-  await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    return {
+      error: 'Database Error: Failed to update invoice.',
+    };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`
-      DELETE FROM invoices
-      WHERE id = ${id}
-    `;
-  revalidatePath('/dashboard/invoices');
+  try {
+    await sql`
+            DELETE FROM invoices
+            WHERE id = ${id}
+          `;
+    revalidatePath('/dashboard/invoices');
+    return {
+      message: 'Invoice deleted successfully.',
+    };
+  } catch (error) {
+    return {
+      error: 'Database Error: Failed to delete invoice.',
+    };
+  }
 }
