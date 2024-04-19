895df9de9e16ccbf76a6b820d7a65df28d1c8601
185182 <185182@cn.chervongroup.com>
2024-04-19 23:02:30 +0800

    feat: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Arce Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-one-omega-91.vercel.app'),
+}
 
 export default function RootLayout({
   children,
